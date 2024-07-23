60f586a243fdfda01fb56044287d5799177d17cf
DhruvaKumar2005 <dumpadhruva07@gmail.com>
2024-07-23 14:25:22 +0530

    changes serailizers.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import ProductListView
+from .views import ProductListView, ServicesListView
 from . import views
 
 urlpatterns = [
     path('api/products/', ProductListView.as_view(), name='product-list'),
     path('api/products/add/', views.add_product, name='add_product'),
+    path('api/services/', ServicesListView.as_view(), name = 'services_list'),
+    path('api/services/add', views.add_service, name = 'add service')
     
 ]
