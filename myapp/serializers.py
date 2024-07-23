ea02e365a7362436d93692eef089510e90d14d3f
DhruvaKumar2005 <dumpadhruva07@gmail.com>
2024-07-23 13:55:46 +0530

    error handling

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Serivices
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
+        fields = '__all__'
+
+
+class ServicesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Serivices
         fields = '__all__'
--- myapp/views.py
+++ myapp/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Services
+from .serializers import ProductSerializer, ServicesSerializer
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 import json
@@ -39,5 +39,49 @@ def add_product(request):
             'Business_name': product.Business_name,
             'description': product.description,
             'price': str(product.price)
+        }, status=201)
+    return JsonResponse({'error': 'Invalid request method'}, status=400)
+
+
+class ServicesListView(APIView):
+    def get(self, request):
+        services = Services.objects.all()
+        serializer = ServicesSerializer(services, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = ServicesSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+@csrf_exempt
+def add_service(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        services = Services.objects.create(
+            serviceName = data['serviceName'],
+            description_ser = data['description_ser'],
+            highestAmount = data['highestAmount'],
+            location = data['location'],
+            lowestAmount = data['lowestAmount'],
+            serviceCategory = data['serviceCategory'],
+            selectedEventTypes = data['selectedEventTypes'],
+            selectedServices = data['selectedServices'],
+            images = data['images'],
+            videos = data['videos']
+        )
+        return JsonResponse({
+            'serviceName' : services.serviceName,
+            'description_ser':services.description_ser,
+            'highestAmount':services.highestAmount,
+            'location':services.location,
+            'lowestAmount':services.lowestAmount,
+            'serviceCategory':services.serviceCategory,
+            'selectedEventTypes':services.selectedEventTypes,
+            'selectedServices':services.selectedServices,
+            'images':services.images,
+            'videos':services.videos
         }, status=201)
     return JsonResponse({'error': 'Invalid request method'}, status=400)
